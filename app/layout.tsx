603f13a05d501bb8b89e2a4dd6742182b2612467
Saeed Altout <saeedaltoutpro@gmail.com>
2024-08-01 17:59:20 +0300

    added sonner component from shadcn/ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 
+import { Toaster } from "@/components/ui/sonner";
+
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700", "800"],
@@ -19,7 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={poppins.className}>{children}</body>
+      <body className={poppins.className}>
+        {children} <Toaster />
+      </body>
     </html>
   );
 }
