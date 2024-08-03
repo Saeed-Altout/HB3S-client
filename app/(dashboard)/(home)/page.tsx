61efe2d4d68357f1d57303d64b8e00d72d981706
Saeed Altout <saeedaltoutpro@gmail.com>
2024-08-03 22:15:06 +0300

    update layout and home page

--- app/(dashboard)/(home)/page.tsx
+++ app/(dashboard)/(home)/page.tsx
@@ -1,3 +1,3 @@
-export default async function HomePage() {
-  return <div>Home</div>;
+export default function HomePage() {
+  return <div>HomePage</div>;
 }
---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,13 @@
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="h-full w-full flex flex-col">
+      Navbar
+      <main className="flex-1 flex flex-col">{children}</main>
+      Footer
+    </div>
+  );
+}
